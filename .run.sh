5bdf8adc54f4d7ab29ff2e774d367483e49ca500
Ryan Meharg <ryanmeharg@gmail.com>
2021-12-17 11:09:13 +0100

    Update run script

--- .run.sh
+++ .run.sh
@@ -31,29 +31,29 @@
   }
 
   if ! ci; then
-    if [ "$(printf '%s' "$(/usr/bin/sw_vers -productVersion)\n10.14" | /usr/bin/sort -V | /usr/bin/head -n 1)" = '10.14' ]; then
-      # Full Disk Access
-      if { ! test -r "${HOME}/Library/Application Support/com.apple.TCC/TCC.db" 2>&1; } | grep -q 'Operation not permitted'; then
-        echo 'Add “Terminal.app” to System Preferences -> Security -> Privacy -> Full Disk Access' 1>&2
-        /usr/bin/open 'x-apple.systempreferences:com.apple.preference.security?Privacy_AllFiles'
-        /usr/bin/open -R /System/Applications/Utilities/Terminal.app
-        exit 1
-      fi
+    # if [ "$(printf '%s' "$(/usr/bin/sw_vers -productVersion)\n10.14" | /usr/bin/sort -V | /usr/bin/head -n 1)" = '10.14' ]; then
+    #   # Full Disk Access
+    #   if { ! test -r "${HOME}/Library/Application Support/com.apple.TCC/TCC.db" 2>&1; } | grep -q 'Operation not permitted'; then
+    #     echo 'Add “Terminal.app” to System Preferences -> Security -> Privacy -> Full Disk Access' 1>&2
+    #     /usr/bin/open 'x-apple.systempreferences:com.apple.preference.security?Privacy_AllFiles'
+    #     /usr/bin/open -R /System/Applications/Utilities/Terminal.app
+    #     exit 1
+    #   fi
 
-      if test -z "$(/usr/bin/sqlite3 "${HOME}/Library/Application Support/com.apple.TCC/TCC.db" \
-                      "SELECT * FROM access WHERE client = 'com.apple.Terminal' AND indirect_object_identifier = 'com.apple.systemevents' AND allowed = 1")"; then
-        /usr/bin/sqlite3 "${HOME}/Library/Application Support/com.apple.TCC/TCC.db" "REPLACE INTO access VALUES('kTCCServiceAppleEvents','com.apple.Terminal',0,1,1,X'fade0c000000003000000001000000060000000200000012636f6d2e6170706c652e5465726d696e616c000000000003',NULL,0,'com.apple.systemevents',X'fade0c000000003400000001000000060000000200000016636f6d2e6170706c652e73797374656d6576656e7473000000000003',0,$(/bin/date +%s))"
-      fi
-    else
-      # Accessibility Access
-      if test -z "$(/usr/bin/sqlite3 '/Library/Application Support/com.apple.TCC/TCC.db' \
-                      "SELECT * FROM access WHERE client = 'com.apple.System/Terminal' AND allowed = 1")"; then
-        echo "\033[0;31mPlease enable Accessibility Access for 'Terminal.app' in System Preferences.\033[0m" 1>&2
-        /usr/bin/open 'x-apple.systempreferences:com.apple.preference.security?Privacy_Accessibility'
-        /usr/bin/open -R /System/Applications/Utilities/Terminal.app
-        exit 1
-      fi
-    fi
+    #   if test -z "$(/usr/bin/sqlite3 "${HOME}/Library/Application Support/com.apple.TCC/TCC.db" \
+    #                   "SELECT * FROM access WHERE client = 'com.apple.Terminal' AND indirect_object_identifier = 'com.apple.systemevents' AND allowed = 1")"; then
+    #     /usr/bin/sqlite3 "${HOME}/Library/Application Support/com.apple.TCC/TCC.db" "REPLACE INTO access VALUES('kTCCServiceAppleEvents','com.apple.Terminal',0,1,1,X'fade0c000000003000000001000000060000000200000012636f6d2e6170706c652e5465726d696e616c000000000003',NULL,0,'com.apple.systemevents',X'fade0c000000003400000001000000060000000200000016636f6d2e6170706c652e73797374656d6576656e7473000000000003',0,$(/bin/date +%s))"
+    #   fi
+    # else
+    #   # Accessibility Access
+    #   if test -z "$(/usr/bin/sqlite3 '/Library/Application Support/com.apple.TCC/TCC.db' \
+    #                   "SELECT * FROM access WHERE client = 'com.apple.System/Terminal' AND allowed = 1")"; then
+    #     echo "\033[0;31mPlease enable Accessibility Access for 'Terminal.app' in System Preferences.\033[0m" 1>&2
+    #     /usr/bin/open 'x-apple.systempreferences:com.apple.preference.security?Privacy_Accessibility'
+    #     /usr/bin/open -R /System/Applications/Utilities/Terminal.app
+    #     exit 1
+    #   fi
+    # fi
 
     # Ask for superuser password, and temporarily add it to the Keychain.
     /usr/bin/security add-generic-password -U -s 'dotfiles' -a "${USER}" -w "$(read -r -s -p "Password: " P < /dev/tty && echo "${P}")"
@@ -106,8 +106,7 @@
   fi
 
   # Run scripts.
-  /usr/bin/bash ./.install-mac
+  /bin/bash ${dotfiles_dir}/.install-mac
 
   at_exit 'echo "\033[0;30mDone.\033[0m"'
-
 }
